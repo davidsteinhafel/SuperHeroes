e89d92164f634e31486bf22c04a1975f131c6756
David steinhafel <davidsteinhafel@outlook.com>
2020-02-14 15:08:43 -0600

    added model hero, added property Dbset<> to my ApplicatinoDbContext class, Added migration, added superheroes controller, and added a global private readonly ApplicationDbContext to my controller class and used dependancy injection.

---
+++ SuperHeroesProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroesProject.Models;
+
+namespace SuperHeroesProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            DbSet<Hero> heroes;
+        }
+        public DbSet<SuperHeroesProject.Models.Hero> Heroes { get; set; }
+    }
+}
---
+++ SuperHeroesProject/Models/Hero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroesProject.Models
+{
+    public class Hero
+    {
+        [Key]
+        public int Id { get; set; }
+        public string name;
+        public string alterEgo;
+        public string primeAbility;
+        public string secondAbility;
+        public string catchphrase;
+        
+    }
+}
